2e97ef15e5b7d134d434a77023c2ca53f115881d
AnDDen <androwd@gmail.com>
2015-12-06 21:29:26 +0400

    CSV parser + writer

--- HydrologyCore/CsvParser/CSVParser.cs
+++ HydrologyCore/CsvParser/CSVParser.cs
@@ -17,43 +17,98 @@ namespace CsvParser
 
     public class CSVParser
     {
-        private const char COMMA = ',';
-        private const char EXP   = '~';
+        private const string COMMA = ",";
+        private const string EXP = "~";
 
-        private const string EOR      = "\n\r";
-        private const char DQUOTE     = '\"';
-        private const string DDQUOTE  = "\"\"";
+        private const string CR = "\r";
+        private const string LF = "\n";
+        private const string CRLF = "\r\n";
 
-        private string str;
-        private int index;
+        private const string DQUOTE = "\"";
+        private const string DDQUOTE = "\"\"";
+        private const string SPACE = " ";
+        private const string TAB = "\t";
 
-        private bool IsMatch(string matchStr)
+        private string[] safechar;
+        private string[] chr;
+        private string[] anychar;
+
+        private string str = null;
+        private int index = 0;
+
+        private void InitChars()
         {
-            if (index + matchStr.Length > str.Length) return false;
-            if (str.Substring(index, matchStr.Length) == matchStr) return true;
-            return false;
+            List<string> list = new List<string>();
+            for (char c = '\u0021'; c < '\u00ff'; c++)
+                if (c != '\u0022' && c != '\u002c')
+                    list.Add(c.ToString());
+            safechar = list.ToArray();
+
+            list.Add(SPACE);
+            chr = list.ToArray();
+
+            list.Add(COMMA);
+            list.Add(DDQUOTE);
+            list.Add(CR);
+            list.Add(LF);
+            list.Add(TAB);
+            anychar = list.ToArray();
         }
 
-        private bool IsMatch(char matchChar)
-        {
-            if (index >= str.Length) return false;
-            return str[index] == matchChar;
-        }
-
-        private void Match(char matchChar)
-        {
-            index++;
-        }
-
-        private void Match(string matchStr)
-        {
-            index += matchStr.Length;
-        }
-
-        public CSVParser(string str)
+        public CSVParser(string str) 
         {
             this.str = str;
-            index = 0;
+            InitChars();
+        }
+
+        private char this[int i]
+        {
+            get
+            {
+                return i < str.Length ? str[i] : (char) 0;
+            }
+        }
+
+        private char Current
+        {
+            get { return this[index]; }
+        }
+
+        private bool End
+        {
+            get { return Current == 0; }
+        }
+
+        private void Next()
+        {
+            if (!End) index++;
+        }
+
+        private string Match(params string[] terms)
+        {
+            int i = index;
+            foreach (string s in terms)
+            {
+                bool match = true;
+                foreach (char c in s)
+                    if (Current == c) Next();
+                    else
+                    {
+                        index = i;
+                        match = false;
+                        break;
+                    }
+                if (match) return s;
+            }
+            return null;
+        }
+
+        private bool IsMatch(params string[] terms)
+        {
+            int i = index;
+            string result = Match(terms);
+            index = i;
+            return result != null;
         }
 
         public DataTable Parse()
@@ -61,10 +116,9 @@ namespace CsvParser
             index = 0;
             DataTable table = new DataTable();
             IList<string> columnNames = ParseRecord();
-            foreach (string column in columnNames) {
+            foreach (string column in columnNames)
                 table.Columns.Add(column);
-            }
-            while (str != "")
+            while (!End)
             {
                 IList<string> row = ParseRecord();
                 DataRow dataRow = table.NewRow();
@@ -78,32 +132,36 @@ namespace CsvParser
         private IList<string> ParseRecord()
         {
             IList<string> record = new List<string>();
-            string fieldColumn = ParseFieldColumn();
-            record.Add(fieldColumn);
-            while (!IsMatch(EOR))
+            record.Add(ParseFieldColumn());
+            while (!IsMatch(CRLF, CR, LF))
             {
-                if (!IsMatch(COMMA))
+                if (IsMatch(COMMA))
                 {
                     Match(COMMA);
-                    fieldColumn = ParseFieldColumn();
-                    record.Add(fieldColumn);
+                    record.Add(ParseFieldColumn());
                 }
+                else throw new CSVParserException("Waited for comma or end of line at position " + index);
             }
-            Match(EOR);
+            Match(CRLF, CR, LF);
             return record;
         }
 
         private string ParseFieldColumn()
         {
-            if (IsMatch(DQUOTE) && !IsMatch(DDQUOTE))
-            {
+            if (IsMatch(DQUOTE))
+            {   
                 Match(DQUOTE);
                 if (IsMatch(EXP)) Match(EXP);
-                string s = ParseFieldPayload();
-                if (!IsMatch(DQUOTE))
-                    throw new CSVParserException(string.Format("Waited for symbol \" at position {0}", index));
-                Match(DQUOTE);
-                return s;
+                string fieldpayload = ParseFieldPayload();
+                if (fieldpayload.Contains(DDQUOTE))
+                    fieldpayload = fieldpayload.Replace(DDQUOTE, DQUOTE);
+                if (IsMatch(DQUOTE))
+                {
+                    Match(DQUOTE);
+                    return fieldpayload;
+                }
+                else 
+                    throw new CSVParserException(string.Format("Waited for symbol {0} at position {1}", DQUOTE, index));
             }
             else
             {
@@ -115,33 +173,27 @@ namespace CsvParser
         private string ParseFieldPayload()
         {
             string s = "";
-            while (!IsMatch(DQUOTE) || IsMatch(DDQUOTE))
-            {
-                if (IsMatch(DDQUOTE)) {
-                    s += DDQUOTE;
-                    Match(DDQUOTE);
-                }
-                else
-                    s += str[index++];
-            }
+            while (IsMatch(anychar))
+                s += Match(anychar);
             return s;
         }
 
         private string ParseRawFieldPayload()
         {
-            // TODO
-
             string s = "";
-            while (!IsMatch(DQUOTE) || IsMatch(DDQUOTE))
+            if (IsMatch(safechar))
+                s += Match(safechar);
+            else throw new CSVParserException("Unexpected symbol at position " + index);
+
+            if (IsMatch(chr) || IsMatch(safechar))
             {
-                if (IsMatch(DDQUOTE))
-                {
-                    s += DDQUOTE;
-                    Match(DDQUOTE);
-                }
-                else
-                    s += str[index++];
+                while (IsMatch(chr))
+                    s += Match(chr);
+
+                if (IsMatch(safechar))
+                    s += Match(safechar);
             }
+
             return s;
         }
     }
---
+++ HydrologyCore/CsvParser/CSVWriter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace CsvParser
+{
+    public class CSVWriter
+    {
+        private const string COMMA = ",";
+        private const string EXP = "~";
+
+        private const string CR = "\r";
+        private const string LF = "\n";
+        private const string CRLF = "\r\n";
+
+        private const string DQUOTE = "\"";
+        private const string DDQUOTE = "\"\"";
+        private const string SPACE = " ";
+        private const string TAB = "\t";
+
+        private static string WriteFieldColumn(string str)
+        {
+            string s = str;
+            if (s.Contains(DQUOTE))
+                s = s.Replace(DQUOTE, DDQUOTE);
+            if (s.Contains(DQUOTE) || s.Contains(COMMA) || s.Contains(CR) || s.Contains(LF) || s.Contains(TAB))
+                s = DQUOTE + s + DQUOTE;
+            return s;
+        }
+
+        public static string Write(DataTable table)
+        {
+            string output = "";
+
+            foreach (DataColumn column in table.Columns)
+                output += WriteFieldColumn(column.ColumnName) + COMMA;
+            output = output.Remove(output.Length - 1, 1);
+            output += CRLF;
+
+            foreach (DataRow row in table.Rows)
+            {
+                foreach (string s in row.ItemArray)
+                    output += WriteFieldColumn(s) + COMMA;
+                output = output.Remove(output.Length - 1, 1);
+                output += CRLF;
+            }
+
+            return output;
+        }
+    }
+}
