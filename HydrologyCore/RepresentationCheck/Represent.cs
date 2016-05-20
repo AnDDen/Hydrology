6dea6e02c9fafa3b329633b4b106912a4693304c
Panda1777 <chizhik1777@rambler.ru>
2016-05-20 22:31:10 +0300

    Representation has been added

---
+++ HydrologyCore/RepresentationCheck/Represent.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoreInterfaces;
+using System.Reflection;
+
+namespace RepresentationCheck
+{
+    [Parameter("n", 20, typeof(Int32))]
+
+    [Name("Расчет коэффициентов")]
+    public class RepresentationCheck : IAlgorithm
+    {
+        private DataSet data;
+        private DataSet resultSet;
+
+        public void Init(DataSet data)
+        {
+            this.data = data;
+        }
+
+        public void Run(IContext ctx)
+        {
+            resultSet = new DataSet();
+            DataTable kTable = new DataTable() { TableName = "Coefficient" };
+            kTable.Columns.Add("K1");
+            kTable.Columns.Add("K2");
+            kTable.Columns.Add("K3");
+            kTable.Columns.Add("K4");
+            kTable.Columns.Add("K5");
+
+            DataTable paramsTable = data.Tables["\\params"];
+            int N = 20;
+            var attrs = typeof(RepresentationCheck).GetCustomAttributes<ParameterAttribute>();
+
+            int n = (int)attrs.First((param) => { return param.Name == "n"; }).DefaultValue;
+
+            foreach (DataRow row in paramsTable.Rows)
+            {
+                switch (row["Name"].ToString())
+                {
+                    case "n":
+                        n = int.Parse(row["Value"].ToString());
+                        break;
+                }
+            }
+            
+            DataTable X_Table = ctx.InitialData.Tables["FlowSequence"];
+
+            List<double> k1 = CoefAvg(X_Table, new statistics(), N);
+
+            for (int i = 0; i < k1.Count; i++)
+            {
+                DataRow row = kTable.NewRow();
+                row["k1"] = k1[i];
+                kTable.Rows.Add(row);
+            }
+            List<double> k2 = CoefSigma(X_Table, new statistics(), N);
+
+            for (int i = 0; i < k2.Count; i++)
+            {
+                DataRow row = kTable.NewRow();
+                row["k2"] = k2[i];
+                kTable.Rows.Add(row);
+            }
+            List<double> k3 = Coef_Cv(X_Table, new statistics(), N);
+
+            for (int i = 0; i < k3.Count; i++)
+            {
+                DataRow row = kTable.NewRow();
+                row["k3"] = k3[i];
+                kTable.Rows.Add(row);
+            }
+            List<double> k4 = Coef_Cs(X_Table, new statistics(), N);
+
+            for (int i = 0; i < k4.Count; i++)
+            {
+                DataRow row = kTable.NewRow();
+                row["k4"] = k4[i];
+                kTable.Rows.Add(row);
+            }
+            List<double> k5 = Coef_Eta(X_Table, new statistics(), N);
+
+            for (int i = 0; i < k5.Count; i++)
+            {
+                DataRow row = kTable.NewRow();
+                row["k5"] = k5[i];
+                kTable.Rows.Add(row);
+            }
+            resultSet.Tables.Add(kTable);
+        }
+
+        public DataSet Results
+        {
+            get { return resultSet; }
+        }
+        //значения из  столбца source
+        public double[] TableValues(DataTable X_Table, int col)
+        {
+            int i = 0;
+            DataColumn column = X_Table.Columns[col];
+            double[] x = new double[X_Table.Rows.Count];
+            foreach (DataRow row in X_Table.Rows)
+            {
+                x[i] = Convert.ToDouble(row[column]);
+                if (i<X_Table.Rows.Count) i++;
+            }
+            return x;
+        }
+        List<double> CoefAvg(DataTable X_Table, statistics stat, int n)
+        {
+            double[] x = TableValues(X_Table, 0);
+            List<double> k1 = new List<double>();
+            double avg = stat.Average(x, 0, x.Length);
+            for (int i = 0; i <= x.Length - n; i++)
+            {
+                k1.Add(stat.Average(x, i, n) / avg);
+            }
+            return k1;
+
+        }
+        List<double> CoefSigma(DataTable X_Table, statistics stat, int n)
+        {
+            double[] x = TableValues(X_Table, 0);
+            List<double> k2 = new List<double>();
+            double sigma = stat.Sigma(x, 0, x.Length);
+            for (int i = 0; i <= x.Length - n; i++)
+            {
+                k2.Add(stat.Sigma(x, i, n ) / sigma);
+            }
+            return k2;
+
+        }
+        List<double> Coef_Cv(DataTable X_Table, statistics stat, int n)
+        {
+            double[] x = TableValues(X_Table, 0);
+            List<double> k3 = new List<double>();
+            double cv = stat.Variation(x, 0, x.Length);
+            for (int i = 0; i <= x.Length - n; i++)
+            {
+                k3.Add(stat.Variation(x, i, n) / cv);
+            }
+            return k3;
+
+        }
+        List<double> Coef_Cs(DataTable X_Table, statistics stat, int n)
+        {
+            double[] q = new double[n];
+            double[] x = TableValues(X_Table, 0);
+            List<double> k4 = new List<double>();
+            double cs = stat.Assimetria(x, 0, x.Length);
+            for (int i = 0; i <= x.Length - n; i++)
+            {
+                k4.Add(stat.Assimetria(x, i, n ) / cs);
+            }
+            return k4;
+
+        }
+        List<double> Coef_Eta(DataTable X_Table, statistics stat, int n)
+        {
+            double[] q = new double[n];
+            double[] x = TableValues(X_Table, 0);
+            List<double> k5 = new List<double>();
+            double cv = stat.Variation(x, 0, x.Length);
+            double cs = stat.Assimetria(x, 0, x.Length);
+            double eta = cs / cv;
+            // int index = 0; 
+            for (int i = 0; i <= x.Length - n; i++)
+            {
+                k5.Add(stat.Assimetria(x, i, n ) / stat.Variation(x, i, n ) / eta);
+            }
+            return k5;
+
+        }
+        [Parameter("type_k", 0, typeof(Int32))]
+
+        [Name("Вероятности превышения")]
+        public class Probability : IAlgorithm
+        {
+            private DataSet data;
+            private DataSet resultSet;
+            
+            public void Init(DataSet data)
+            {
+                this.data = data;
+            }
+
+            public void Run(IContext ctx)
+            {
+                resultSet = new DataSet();
+                DataTable pTable = new DataTable() { TableName = "Probability" };
+                DataTable K_Table = ctx.InitialData.Tables["Coefficient"];
+                pTable.Columns.Add("p");
+            //   pTable.Columns.Add("k");
+                RepresentationCheck represent = new RepresentationCheck();
+
+                DataTable paramsTable = data.Tables["\\params"];
+
+                var attrs = typeof(Probability).GetCustomAttributes<ParameterAttribute>();
+
+                int type_k = (int)attrs.First((param) => { return param.Name == "type_k"; }).DefaultValue;
+
+                foreach (DataRow row in paramsTable.Rows)
+                {
+                    switch (row["Name"].ToString())
+                    {
+                        case "type_k":
+                            type_k = int.Parse(row["Value"].ToString());
+                            break;
+                    }
+                }
+                statistics stat = new statistics();
+                double [] k = represent.TableValues(K_Table,type_k);
+                k = stat.sort_shell(k, k.Length);
+                double [] p = stat.probability(k);
+                for (int i = 0; i < p.Length; i++)
+                       {
+                           DataRow row = pTable.NewRow();
+                           row["p"] = p[i];
+                           pTable.Rows.Add(row);
+                       }
+        /*        for (int i = 0; i < k.Length; i++)
+            {
+                DataRow row = pTable.NewRow();
+                row["k"] = k[i];
+                pTable.Rows.Add(row);
+            }*/
+            resultSet.Tables.Add(pTable);
+            }
+            
+            public DataSet Results
+            {
+                get { return resultSet; }
+            }
+
+
+        }
+
+        [Parameter("type_k", 0, typeof(Int32))]
+        [Name("Стандарты")]
+        public class Standarts : IAlgorithm
+        {
+            private DataSet data;
+            private DataSet resultSet;
+
+            public void Init(DataSet data)
+            {
+                this.data = data;
+            }
+
+            public void Run(IContext ctx)
+            {
+                resultSet = new DataSet();
+                DataTable K_Table = ctx.InitialData.Tables["Coefficient"];
+                DataTable SigmaTable = new DataTable() { TableName = "Standarts" };
+                SigmaTable.Columns.Add("K");
+                SigmaTable.Columns.Add("Sigma");
+                DataTable paramsTable = data.Tables["\\params"];
+                var attrs = typeof(Probability).GetCustomAttributes<ParameterAttribute>();
+                int type_k = (int)attrs.First((param) => { return param.Name == "type_k"; }).DefaultValue;
+                RepresentationCheck represent = new RepresentationCheck();
+                statistics stat = new statistics();
+                foreach (DataRow row in paramsTable.Rows)
+                {
+                    switch (row["Name"].ToString())
+                    {
+                        case "type_k":
+                            type_k = int.Parse(row["Value"].ToString());
+                            break;
+                    }
+                }
+                double[] k = represent.TableValues(K_Table, type_k);
+                double sigma = stat.standart(k);
+
+            }
+
+            public DataSet Results
+            {
+                get { return resultSet; }
+            }
+        }
+    }
+}
+
+
---
+++ HydrologyCore/RepresentationCheck/statistics.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepresentationCheck
+{
+    public class statistics
+    {
+       // public statistics(double [] x)
+      //  {
+             
+      //  }
+
+        //выборочное среднее
+        public double Average(Double[] x, int i0, int n)
+        {
+            double s = 0;
+            int ik = i0 + n ;
+            for (int i = i0; i < ik; i++)
+                s += x[i];
+            return s / n;
+        }
+        public double Average(double [] k)
+        {
+            double s = 0;
+            for (int i = 0; i < k.Length; i++)
+                s += k[i];
+            return s / k.Length;
+        }
+
+
+        //среднеквадратичное отклонение
+        public double Sigma(Double[] x, int i0, int n)
+        {
+            int ik;
+            double s, avr;
+            if (n == 1) return 0;
+            else
+            {
+                avr = Average(x, i0, n);
+                s = 0;
+                ik = i0 + n ;
+                for (int i = i0; i < ik; i++)
+                    s += Math.Pow(x[i] - avr, 2);
+                return Math.Sqrt(s / (n - 1));
+            }
+        }
+
+        //Коэффициент вариации
+        public double Variation(Double[] x, int i0, int n)
+        {
+            double s = Sigma(x, i0, n), avr = Average(x, i0, n);
+            if (avr != 0)
+                return s / avr;
+            else
+                return 0;
+        }
+
+        //коэффициент ассиметрии
+        public double Assimetria(Double[] x, int i0, int n)
+        {
+            double sig, avr, s = 0;
+            int ik, i;
+            if ((n == 1) | (n == 2)) return 0;
+            else
+            {
+                avr = Average(x, i0, n);
+                sig = Sigma(x, i0, n);
+                ik = i0 + n - 1;
+                for (i = i0; i <= ik; i++)
+                    s += Math.Pow(x[i] - avr, 3);
+                return s * n / ((n - 1) * (n - 2) * Math.Pow(sig, 3));
+            }
+        }
+
+        //Коэффициент корреляции
+        public double Correlation(Double[] x, int i0, int n)
+        {
+            double avr1 = Average(x, i0, n - 1),
+                avr2 = Average(x, i0 + 1, n),
+                s1 = 0, s2 = 0, s3 = 0;
+            int ik = i0 + n - 2, i;
+            for (i = i0; i <= ik; i++)
+            {
+                s1 += (x[i] - avr1) * (x[i + 1] - avr2);
+                s2 += Math.Pow(x[i] - avr1, 2);
+                s3 += Math.Pow(x[i + 1] - avr2, 2);
+            }
+            return s1 / Math.Pow(s2 * s3, 2);
+        }
+        //стандарты
+        public double standart(double [] k)
+        {
+            double s = 0;
+            double avg_k = Average(k);
+            for (int i = 0; i < k.Length; i++)
+                s += Math.Pow(k[i] - avg_k,2);
+            return Math.Sqrt(s / k.Length);
+
+        }
+        public double[] probability(double[] k)
+        {
+            int N = k.Length;
+            double[] p = new double[N];
+            for (int i = 0; i < N; i++)
+                p[i] = (double)(i + 1) / (double)(N + 1);
+            return p;
+        }
+        
+
+        //сортировка
+         public double [] sort_shell(double [] a,  int N)             
+        {
+            double tmp;
+            for (int gap = N / 2; gap > 0; gap /= 2)
+                for (int i = gap; i < N; i++)
+                    for (int j = i - gap; j >= 0 && (a[j + gap] > a[j]); j -= gap)
+                    {
+                        tmp = a[j];
+                        a[j] = a[j+gap];
+                        a[j+gap] = tmp;
+                    }
+            return a;
+        }      
+
+    }
+}
